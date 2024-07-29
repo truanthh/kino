2f499b4ab0895dac2d2455a170f5e0163e6e5e02
truanthh <aquapyr@gmail.com>
2024-07-29 21:22:41 +0300

    ready to go
    all cleaned up, set up router and pinia, ready to go!

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+
+const data = ref("heyyo");
+
+// function debounce(func, delay) {
+//   let timeoutID;
+//
+//   return function (...args) {
+//     if (timeoutID) {
+//       clearTimeout(timeoutID);
+//     }
+//
+//     timeoutID = setTimeout(() => {
+//       func.apply(this, args);
+//     }, delay);
+//   };
+// }
+</script>
+
+<template>
+  <div>
+    <input class="myinput" v-model="data" />
+    <h1>{{ data }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.myinput {
+  width: 200px;
+  height: 40px;
+  font-size: 20px;
+  padding: 0px 8px 0px 8px;
+}
+</style>

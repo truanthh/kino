0ef349e7dd0ccbe764c9687510c23952cb6e83e1
truanthh <aquapyr@gmail.com>
2024-08-21 13:55:45 +0300

    scss

--- src/App.vue
+++ src/App.vue
@@ -28,55 +28,13 @@ checkUser();
   </main>
 </template>
 
-<style>
-:root {
-  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-  line-height: 1.5;
-  font-weight: 400;
-
-  color-scheme: light dark;
-  color: rgba(255, 255, 255, 0.87);
-  background-color: #242424;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-html,
-body {
-  height: 100%;
-  margin: 0;
-  box-sizing: border-box;
-}
-main {
-  /* flex: 1; */
-  height: calc(100% - 3rem);
-  overflow-y: auto;
-}
+<style lang="scss">
+@import "./styles/global.scss";
 
 #app {
   height: 100%;
   width: 100%;
   /* display: flex; */
   /* flex-direction: column; */
-}
-
-* {
-  box-sizing: border-box;
-  /* margin: 0; */
-  /* padding: 0; */
-}
-@media (prefers-color-scheme: light) {
-  :root {
-    color: #213547;
-    background-color: #ffffff;
-  }
-  a:hover {
-    color: #747bff;
-  }
-  button {
-    background-color: #f9f9f9;
-  }
 }
 </style>
